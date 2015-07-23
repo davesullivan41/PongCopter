809492cb426d9fc84669f8f6c3474423f14fa852
David Sullivan <davesullivan41@gmail.com>
2015-07-23 18:35:57 -0500

    quick.js now actually tracks the ball. All motion changes should occur in updateDroneAction, which is passed a set of 5 x,y pairs. If there are less than 5, the coordinates will be set to -1

--- detectionAlgorithm/findBall.js
+++ detectionAlgorithm/findBall.js
@@ -2,31 +2,39 @@ var cv = require('../node/node_modules/opencv/lib/opencv');
 
 // (B)lue, (G)reen, (R)ed
 // 250, 120, 195
-var lower_threshold = [50, 0, 0];
-var upper_threshold = [230, 130, 25];
-var minArea = 2000;
+var lower_threshold = [30, 130, 190];
+var upper_threshold = [100, 205, 255];
+var lower_hsv = [10, 150, 150]
+var upper_hsv = [25, 255, 255]
+var minArea = 200;
 var WHITE = [255, 255, 255]; // B, G, R
 
-for(j = 0 ; j < 5 ; j++){
-	cv.readImage('./files/' + j.toString() + '.jpg', function(err, im) {
+for(j = 1 ; j < 221 ; j++){
+	cv.readImage('../flightTest/RobBounce/raw' + j.toString() + '.jpeg', function(err, im) {
 	  if (err) throw err;
 	  if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
-	  im.inRange(lower_threshold, upper_threshold);
+	  //im.dilate(2);
+	  var hsv = new cv.Matrix(im.height(), im.width());
+	  hsv = im.copy();
+	  hsv.convertHSVscale();
+	  hsv.inRange(lower_hsv, upper_hsv);
 
 	  var out = new cv.Matrix(im.height(), im.width());
-	  im.dilate(8);
 
-	  contours = im.findContours();
+	  hsv.dilate(2);
+
+	  contours = hsv.findContours();
 
 	  for (i = 0; i < contours.size(); i++) {
 
 	    if (contours.area(i) < minArea) continue;
-
-	    var arcLength = contours.arcLength(i, true);
+	   	//console.log("x: " + contours.boundingRect(i).x + " y: " + contours.boundingRect(i).y);
+	    //var arcLength = contours.arcLength(i, true);
 	    //contours.approxPolyDP(i, 0.01 * arcLength, true);
 	    out.drawContour(contours, i, WHITE);
 	   }
+	  console.log(j + " complete");
 
-	  out.save('./out/' + j.toString() + '.jpg');
+	  out.save('./out/hsv' + j.toString() + '.jpeg');
 	});
 }
--- flightTest/quick.js
+++ flightTest/quick.js
@@ -1,26 +1,89 @@
 var arDrone = require('../node/node_modules/ar-drone');
 var cv = require('../node/node_modules/opencv');
-var client = arDrone.createClient();
-var imageIndex = 1;
-var lower_threshold = [50, 0, 0];
-var upper_threshold = [230, 130, 25];
+var drive = require('./droneDrive.js');
 
+// create the ardrone client
+var client = arDrone.createClient(framerate=15);
+
+// hsv color window
+var lower_hsv = [10, 150, 150]
+var upper_hsv = [25, 255, 255]
+
+function updateDroneAction(coords)
+{
+  if(coords[0][0] == -1)
+  {
+    console.log("I DONT SEE IT")
+    client.stop();
+  }
+  else if(coords[0][0] > 370)
+  {
+    console.log("I see the ball to the right")
+    client.right(0.2);
+        client.after(50,function(){
+       this.stop()
+      });
+  }
+  else if(coords[0][0] < 350)
+  {
+    console.log("I see the ball to the left")
+    client.left(0.2);
+        client.after(50,function(){
+       this.stop();
+      });
+  }
+  else
+  {
+    console.log("I DONT SEE IT")
+    client.stop();
+  }
+}
+
+// image stream
 var pngStream = client.getPngStream();
+
+// image stream event handler
 pngStream.on('data', function(image){
+  // pass the image to opencv
   cv.readImage(image, function(err, im){
     if(!err){
-      im.save('training/raw' + imageIndex + '.jpeg');
-      im.dilate(1);
-      im.inRange(lower_threshold, upper_threshold);
-      im.save('trainings/filtered' + imageIndex + '.jpeg');
-      imageIndex = imageIndex + 1;
+      // optionally save the raw images
+      //im.save('log/raw' + imageIndex + '.jpeg');
+
+      // convert to hsv and filter out the ball
+      im.convertHSVscale();
+      im.inRange(lower_hsv, upper_hsv);
+
+      // optionally save the current image
+      im.save('curr.jpeg');
+
+      // find the contours
+      contours = im.findContours();
+      
+      // store up to the first 5 coordinates in an array
+      coordinates = new Array();
+      for(var i=0;i<5;i++)
+      {
+        if(i >= contours.size())
+        {
+          coordinates.push([-1,-1]);
+        }
+        else
+        {
+          coordinates.push([contours.boundingRect(i).x,contours.boundingRect(i).y]);
+        }
+      }
+
+      // pass the array and the drone controller to a separate function
+      updateDroneAction(coordinates);
     }
   })
 });
 
+// take off and fly for 2 minutes
 client.takeoff();
 client
-  .after(15000, function(){
+  .after(1200000, function(){
     this.land()
     this.stop()
   });
