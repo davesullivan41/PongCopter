5f7714eabc28e8a2239906c4e2abe7e2e70af93a
David Sullivan <davesullivan41@gmail.com>
2015-07-30 08:07:28 -0500

    Added opencv and some modifications to quick

--- flightTest/quick.js
+++ flightTest/quick.js
@@ -2,7 +2,7 @@ var arDrone = require('../node/node_modules/ar-drone');
 var cv = require('../node/node_modules/opencv');
 
 // create the ardrone client
-var client = arDrone.createClient(framerate=5); 
+var client = arDrone.createClient(framerate=30); 
 // currently 640 x 480... which is ridiculously low, and could be explaining our issues
 
 var imgCount = 0;
@@ -10,6 +10,9 @@ var imgCount = 0;
 // hsv color window
 var lower_hsv = [10, 150, 150]
 var upper_hsv = [25, 255, 255]
+
+var lastPng;
+var processingImage = false;
 
 function updateDroneAction(coords)
 {
@@ -22,8 +25,8 @@ function updateDroneAction(coords)
   {
     console.log("I see the ball to the right")
     //client.right(0.5*(coords[0][0]-320.0)/320.0);
-    client.right(0.2);
-        client.after(45,function(){
+   client.right(0.2);
+        client.after(50,function(){
         client.left(0.2);
       });
   }
@@ -32,7 +35,7 @@ function updateDroneAction(coords)
     console.log("I see the ball to the left")
     client.left(0.2);
     //client.left(0.5*coords[0][0]/320.0);
-        client.after(45,function(){
+      client.after(50,function(){
        client.right(0.2);
       });
   }
@@ -40,6 +43,50 @@ function updateDroneAction(coords)
   {
     console.log("I DONT SEE IT")
     client.stop();
+  }
+  processingImage = false;
+}
+
+function processImage(){
+  if( (! processingImage ) && lastPng){
+    processingImage = true;
+    cv.readImage(lastPng, function(err, im){
+      if(!err){
+        // optionally save the raw images
+        //im.save('log/raw' + imgCount + '.jpeg');
+
+        // convert to hsv and filter out the ball
+        im.convertHSVscale();
+        im.inRange(lower_hsv, upper_hsv);
+        im.dilate(3);
+        
+
+        // find the contours
+        contours = im.findContours();
+        
+        // store up to the first 5 coordinates in an array
+        coordinates = new Array(5);
+        for(var i=0;i<5;i++)
+        {
+          if(i >= contours.size())
+          {
+            coordinates[i] = [-1,-1,-1];
+          }
+          else
+          {
+            coordinates[i] = [contours.boundingRect(i).x,contours.boundingRect(i).y,contours.area(i)];
+          }
+        }
+        //var end = new Date().getTime();
+        //console.log(end - start);
+        // pass the array and the drone controller to a separate function
+        updateDroneAction(coordinates);
+
+        // optionally save the current image
+        //im.save('log/proc' + imgCount + '.jpeg');
+        //imgCount = imgCount + 1;
+      }
+    })
   }
 }
 
@@ -50,45 +97,10 @@ var pngStream = client.getPngStream();
 pngStream.on('data', function(image){
   //var start = new Date().getTime();
   // pass the image to opencv
-  cv.readImage(image, function(err, im){
-    if(!err){
-      // optionally save the raw images
-      //im.save('log/raw' + imgCount + '.jpeg');
-
-      // convert to hsv and filter out the ball
-      im.convertHSVscale();
-      im.inRange(lower_hsv, upper_hsv);
-      im.dilate(3);
-      
-
-      // find the contours
-      contours = im.findContours();
-      
-      // store up to the first 5 coordinates in an array
-      coordinates = new Array(5);
-      for(var i=0;i<5;i++)
-      {
-        if(i >= contours.size())
-        {
-          coordinates[i] = [-1,-1,-1];
-        }
-        else
-        {
-          coordinates[i] = [contours.boundingRect(i).x,contours.boundingRect(i).y,contours.area(i)];
-        }
-      }
-      //var end = new Date().getTime();
-      //console.log(end - start);
-      // pass the array and the drone controller to a separate function
-      updateDroneAction(coordinates);
-
-      // optionally save the current image
-      
-      im.save('log/proc' + imgCount + '.jpeg');
-      imgCount = imgCount + 1;
-    }
-  })
+  lastPng = image;
 });
+
+var detectBall = setInterval(processImage,20);
 
 // take off and fly for 2 minutes
 client.takeoff();
--- flightTest/tcpTest.js
+++ flightTest/tcpTest.js
@@ -2,9 +2,9 @@ var arDrone = require('../node/node_modules/ar-drone');
 var cv = require('../node/node_modules/opencv');
 
 // create the ardrone client
-var client = arDrone.createUdpControl();//framerate=15); 
 // currently 640 x 360... which is ridiculously low, and could be explaining our issues
-
+//var UdpControl = require('../node/node_modules/ar-drone/lib/control/UdpControl');
+var client = arDrone.createClient();
 var PaVEParser = require('../node/node_modules/ar-drone/lib/video/PaVEParser');
 //var stream = new cv.ImageDataStream();
 
@@ -12,49 +12,36 @@ var imgCount = 0;
 
 //var output = require('fs').createWriteStream('./vid.h264');
 
-var video = client.getVideoStream();
-var parser = new PaVEParser();
-
-/*
-stream.on('frame', function(matrix){
-  console.log('frame');
-  console.log(matrix.length);
-
-});*/
-
-parser
-  .on('data', function(data) {
-   cv.readImage(matrix, function(err, im){
-    if(!err){
-      console.log("saving image");
-      im.save('log/tcp' + imgCount + '.jpeg');
-      imgCount = imgCount + 1;
-    }
-  })
-  })
-  .on('end', function() {
-    console.log('end');
-    s.end();
-  });
-
-
-video.pipe(parser);
-
-
-
-/*
 
 // image stream
 var tcpStream = client.getVideoStream();
 
-var imgSize = 640 * 360;
-var pixelCount = 0
+//var imgSize = 640 * 360;
+//var pixelCount = 0
 
-var out = new cv.Matrix(360,640);
+//var out = new cv.Matrix(360,640);
 
+var parser = new PaVEParser();
+
+parser
+  .on('data', function(data) {
+    cv.readImage(data.payload, function(err, im){
+      if(!err){
+        console.log("saving image");
+        im.save('log/tcp' + imgCount + '.jpeg');
+        imgCount = imgCount + 1;
+      }
+    })
+  })
+  .on('end', function() {
+    console.log('end');
+  });
+
+tcpStream.pipe(parser);
+/*
 // image stream event handler
 tcpStream.on('data', function(image){
-  console.log(image.length);
+  //console.log(image.length);
   //var mat = new cv.Matrix()
   //console.log(640*360);
   // console.log(image.readUInt8(0));
@@ -62,18 +49,78 @@ tcpStream.on('data', function(image){
   // console.log(image.readUInt8(2));
   console.log('---');
   // pass the image to opencv
- cv.readImage(image, function(err, im){
+  cv.readImage(image, function(err, im){
     if(!err){
       im.save('log/tcp' + imgCount + '.jpeg');
       imgCount = imgCount + 1;
     }
   })
-});
-*/
+});*/
+
 // take off and fly for 10 seconds
 client.takeoff();
 client
-  .after(6000, function(){
+  .after(8000, function(){
     this.land()
     this.stop()
   });
+
+
+
+
+/// UDP Raw stuff
+/*
+var fly       = true;
+var emergency = true;
+
+
+setInterval(function() {
+  //control.ref({fly: fly, emergency: emergency});
+  client.raw('CONFIG',605,'video:video_codec','80');
+  client.raw('REF', (1000000000) )
+  console.log("hovering");
+  client.pcmd();
+ client.flush();
+}, 300);
+
+
+var tcpStream = require('../node/node_modules/ar-drone/lib/video/TcpVideoStream');
+var video = new tcpStream();
+var callback = function(err) {
+  if (err) {
+    console.log('TcpVideoStream error: %s', err.message);
+    setTimeout(function () {
+      console.log('Attempting to reconnect to TcpVideoStream...');
+      video.connect(callback);
+    }, 1000);
+  }
+};
+
+var parser = new PaVEParser();
+
+parser
+  .on('data', function(data) {
+    cv.readImage(data.payload, function(err, im){
+      if(!err){
+        console.log("saving image");
+        im.save('log/tcp' + imgCount + '.jpeg');
+        imgCount = imgCount + 1;
+      }
+    })
+  })
+  .on('end', function() {
+    console.log('end');
+  });
+
+video.pipe(parser);
+
+
+// For the first second, disable emergency if there was one
+setTimeout(function() {
+  emergency = false;
+}, 1000);
+
+setTimeout(function() {
+  fly = false;
+}, 5000);
+*/
